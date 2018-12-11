1170167022c58b70f3d80223946b6923cc271010
Bruno Santos <brsantos@gmail.com>
2018-12-11 09:52:32 -0500

    Exception endpoint (REST) /categorias/{id}

--- src/main/java/com/brunosantos/cursomc/services/CategoriaService.java
+++ src/main/java/com/brunosantos/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.brunosantos.cursomc.domain.Categoria;
 import com.brunosantos.cursomc.repositories.CategoriaRepository;
+import com.brunosantos.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository rep;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = rep.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
+
 }
